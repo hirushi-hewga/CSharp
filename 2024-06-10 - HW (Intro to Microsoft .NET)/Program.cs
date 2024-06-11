7e6496ebf9d148314d2f8a75cc4c87ca25abd129
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-06-11 11:04:04 +0300

    HW (IntroToMicrosoft.NET)

---
+++ 2024-06-10 - HW (Intro to Microsoft .NET)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Specialized;
+
+namespace _2024_06_10___HW__Intro_to_Microsoft_.NET_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("It's easy to win forgiveness for being wrong;");
+            //Console.WriteLine("being right is what gets you into real trouble.");
+            //Console.WriteLine("Bjarne Stroustrup");
+
+
+            Console.Write("Enter number : ");
+            Console.ReadLine();
+            int a;
+            int min;
+            int max;
+            int sum = 0;
+            int dobutok = 1;
+            for (int i = 0; i < 4; i++)
+            {
+                
+            }
+        }
+    }
+}
