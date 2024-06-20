e7730d9dba534c01aa614c143cf8c9659fcdc767
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-06-20 13:18:41 -0700

    Lesson (IntroToMicrosoft.NET)

--- 2024-06-10 - Lesson (Intro to Microsoft .NET)/Program.cs
+++ 2024-06-10 - Lesson (Intro to Microsoft .NET)/Program.cs
@@ -1,45 +1,63 @@
-﻿namespace _2024_06_10___Lesson__Intro_to_Microsoft_.NET_
+﻿using System.Data;
+
+namespace _2024_06_10___Lesson__Intro_to_Microsoft_.NET_
 {
     class Program
     {
         static void Main(string[] args)
         {
+            /*
             object obj = new object();
 
             byte a = 255;
+            byte b = 5;
 
+            short c = 3;
 
-            Byte b = 5;
-            //Console.WriteLine(a);
-
-
-
-
-
-            Console.WriteLine("Hello, World!");
+            int s = 4;
+            */
+            /*
+            Console.WriteLine("Hello world");
             Console.Write("Hello\n");
             Console.Write("\tHello\n");
             Console.Write("\t\tHello");
             Console.Write("Hello");
-            Console.Write("Hello");
-            Console.Write("Hello");
             Console.Write("Hello\n");
 
-            Console.WriteLine("Enter number : ");
+            Console.Write("Enter number : ");
             string str = Console.ReadLine();
 
             int number = int.Parse(str);
             Console.WriteLine(str + "!!!!!");
             Console.WriteLine(str + 100);
             Console.WriteLine(number + 100);
-            Console.WriteLine("You entered ", number + 100, "!!!!!");
-            Console.WriteLine($"You entered {number + 100} !!!!!");
+            Console.WriteLine("You entered " + number + 100 + "!!!!!");
+            Console.WriteLine($"You entered { number + 100 }!!!!!");
+            */
+            /*
+            // Not nullable
+            int a = 0;
 
-
-            //int d = 0;
-            //int? r = null;
             // int* ptr = nullptr;
-            // int* ptr = new int(5)
+            // int* ptr = new int(5);
+            // int r = null; // error
+
+
+            // Nullable
+            // Nullable<int> r = null;
+            int? r = null;
+            */
+            /*
+            DateTime now = DateTime.Now;
+            Console.WriteLine($"Custom date : {now.ToString("yyyy-MM-dd")}");
+            Console.WriteLine($"Custom time : {now.ToString("HH-mm-ss")}");
+            */
+            /*
+            string str = null;
+            if (str != null)
+                str.ToUpper();
+            string str2 = str ?? "Empty"; // null conditional operator
+            */
         }
     }
 }
