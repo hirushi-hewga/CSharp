b7d55d36b4898d745d1d91228524b9e1ca7a5302
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-06-21 00:15:52 -0700

    HW (IntroToMicrosoft.NET)

--- 2024-06-10 - HW (Intro to Microsoft .NET)/Program.cs
+++ 2024-06-10 - HW (Intro to Microsoft .NET)/Program.cs
@@ -6,16 +6,35 @@ namespace _2024_06_10___HW__Intro_to_Microsoft_.NET_
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("It's easy to win forgiveness for being wrong;");
-            //Console.WriteLine("being right is what gets you into real trouble.");
-            //Console.WriteLine("Bjarne Stroustrup");
+            /*
+            Console.WriteLine("It's easy to win forgiveness for being wrong;");
+            Console.WriteLine("being right is what gets you into real trouble.");
+            Console.WriteLine("Bjarne Stroustrup");
+            */
 
 
+            /*
+            int suma = 0;
+            int dobutok = 1;
+            int? min = null;
+            int? max = null;
             int a;
-            for (int i = 0;i < args.Length; i++)
+            Console.WriteLine("--- Enter five numbers ---");
+            for (int i = 0; i < 5; i++)
             {
+                Console.Write("Enter number : ");
                 a = int.Parse(Console.ReadLine());
+                if (min == null) min = max = a;
+                if (min > a) min = a;
+                if (max < a) max = a;
+                suma += a;
+                dobutok *= a;
             }
+            Console.WriteLine($"\nDobutok : {dobutok}");
+            Console.WriteLine($"Suma : {suma}");
+            Console.WriteLine($"Min : {min}");
+            Console.WriteLine($"Max : {max}");
+            */
         }
     }
 }
