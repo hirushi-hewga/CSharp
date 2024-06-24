f9c005be88039fc71f21d16e21d3e3fd7ef3a517
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-06-24 14:32:08 -0700

    Enum

---
+++ 2024-06-14 - Lesson (Enum)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _2024_06_14___Lesson__Enum_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
