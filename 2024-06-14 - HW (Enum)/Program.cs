b21d67e8ac14c75316a30ddd91a36d75158ec6db
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-06-25 16:15:05 -0700

    HW (String)

--- 2024-06-14 - HW (Enum)/Program.cs
+++ 2024-06-14 - HW (Enum)/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace _2024_06_14___HW__Enum_
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Threading.Channels;
+
+namespace _2024_06_14___HW__Enum_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region 1
+
+            /*
+            string str_1 = "Hello, !";
+            string str_2 = "World";
+            int pos = 7;
+            Console.WriteLine(str_1);
+            Console.WriteLine(str_2);
+            StringBuilder str_3 = new StringBuilder(str_1);
+            str_3.Insert(pos, str_2);
+            Console.WriteLine(str_3);
+            */
+
+            #endregion
+
+            #region 2
+
+            /*
+            Console.Write("Enter word : ");
+            string word = Console.ReadLine();
+            bool isPalindrome = true;
+            for (int i = 0; i < word.Length / 2; i++)
+            {
+                if (word[i] != word[word.Length - i - 1])
+                    isPalindrome = false;
+            }
+            if (isPalindrome)
+                Console.WriteLine("\nThis word is palindrome");
+            else
+                Console.WriteLine("\nThis word is not palindrome");
+            */
+
+            #endregion
         }
     }
 }
