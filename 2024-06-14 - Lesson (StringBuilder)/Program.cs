461eebeaa02627552986ab3a281ccb622c004631
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-06-25 15:34:19 -0700

    Lesson (StringBuilder)

---
+++ 2024-06-14 - Lesson (StringBuilder)/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+
+namespace _2024_06_14___Lesson__StringBuilder_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string str = "hello";
+            str += "hello";
+            str += "hello";
+            str += "hello";
+            str += "hello";
+            str += "hello";
+            str += "hello";
+            str += "hello";
+            str += "hello";
+            str += "hello";
+            str += "hello";
+            str += "hello";
+            str += "hello";
+            str += "hello";
+            str += "hello";
+            */
+
+
+            string str = "hello";
+            str += "hello";
+
+            StringBuilder builder = new StringBuilder();
+            Console.WriteLine($"Length : {builder.Length}");
+            Console.WriteLine($"Capacity : {builder.Capacity}");
+
+            builder.Append("Hello");
+            builder.Append("Hello");
+            builder.Append("Hello");
+            Console.WriteLine($"Length : {builder.Length}");
+            Console.WriteLine($"Capacity : {builder.Capacity}");
+            
+            builder.AppendLine("World");
+            Console.WriteLine($"Length : {builder.Length}");
+            Console.WriteLine($"Capacity : {builder.Capacity}");
+
+
+            Console.WriteLine(builder);
+            builder.Append("Hello");
+            builder.Append("Hello");
+            builder.Append("Hello");
+            Console.WriteLine($"Length : {builder.Length}");
+            Console.WriteLine($"Capacity : {builder.Capacity}");
+            Console.WriteLine(builder);
+        }
+    }
+}
