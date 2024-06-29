533b10a692378e0f6651d8b43b1f6b90599c04d4
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-06-29 15:58:34 -0700

    HW (Intro_To_OOP)

--- 2024-06-17 - HW (Intro to OOP)/Freezer.cs
+++ 2024-06-17 - HW (Intro to OOP)/Freezer.cs
@@ -6,5 +6,72 @@ using System.Threading.Tasks;
 
 namespace _2024_06_17___HW__Intro_to_OOP_
 {
-    
+    partial class Freezer
+    {
+        public Freezer()
+        {
+            model = null;
+            color = null;
+            Volume = 20;
+        }
+        public Freezer(string model)
+        {
+            Model = model;
+            color = null;
+        }
+        public Freezer(string model, string color) : this(model)
+        {
+            Color = color;
+        }
+    }
+    partial class Freezer
+    {
+        public string Model
+        {
+            get {
+                if (model == null)
+                    return "no model";
+                return model;
+            }
+            private set { model = value; }
+        }
+        public string Color
+        {
+            get {
+                if (color == null)
+                    return "no model";
+                return color;
+            }
+            set { color = value; }
+        }
+        public int Volume
+        {
+            get { return volume; }
+            set
+            {
+                if (value > 0)
+                    volume = value;
+                else
+                    volume = Math.Abs(value);
+            }
+        }
+        public int Min_t
+        {
+            get { return min_t; }
+            set
+            {
+                if (value <= 0)
+                    min_t = value;
+            }
+        }
+        public int Max_t
+        {
+            get { return max_t; }
+            set
+            {
+                if (value <= 0)
+                    max_t = value;
+            }
+        }
+    }
 }
--- 2024-06-17 - HW (Intro to OOP)/Program.cs
+++ 2024-06-17 - HW (Intro to OOP)/Program.cs
@@ -3,71 +3,23 @@ using System.Reflection;
 
 namespace _2024_06_17___HW__Intro_to_OOP_
 {
-    class Freezer
+    partial class Freezer
     {
         private string model;
         private string color;
         private int volume;
         private int min_t;
         private int max_t;
-
-        public Freezer()
-        {
-            model = "no model";
-            color = "no color";
-            Volume = 20;
-            Min_t = 0;
-            Max_t = 0;
-        }
-        public Freezer(string model) : base()
-        {
-            this.model = model;
-        }
-        public Freezer(string model, string color) : this(model)
-        {
-            this.color = color;
-        }
-
-        public string Model { get; }
-        public string Color { get; set; }
-        public int Volume
-        {
-            get { return volume; }
-            set
-            {
-                if (value > 0)
-                    volume = value;
-            }
-        }
-        public int Min_t
-        {
-            get { return min_t; }
-            set
-            {
-                if (value <= 0)
-                    min_t = value;
-            }
-        }
-        public int Max_t
-        {
-            get { return max_t; }
-            set
-            {
-                if (value <= 0)
-                    max_t = value;
-            }
-        }
-
         public override string ToString()
         {
-            return $"Model : {model}, Color : {color}, Volume : {volume}, Min temperature : {min_t}, Max temperature : {max_t}";
+            return $"Model : {Model} ,  Color : {Color} ,  Volume : {Volume} ,  Min temperature : {Min_t} ,  Max temperature : {Max_t}";
         }
     };
     class Program
     {
         static void Main(string[] args)
         {
-            Freezer freezer = new Freezer("freezer", "black");
+            Freezer freezer = new Freezer("freezer", "Black");
             string string_freezer = freezer.ToString();
             Console.WriteLine(string_freezer);
         }
