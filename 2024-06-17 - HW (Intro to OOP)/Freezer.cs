d8c60843e452567308411c17bbb95ad7975d50a9
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-06-29 06:26:28 -0700

    HW (Intro_To_OOP)

--- 2024-06-17 - HW (Intro to OOP)/Freezer.cs
+++ 2024-06-17 - HW (Intro to OOP)/Freezer.cs
@@ -6,30 +6,5 @@ using System.Threading.Tasks;
 
 namespace _2024_06_17___HW__Intro_to_OOP_
 {
-    internal class Freezer
-    {
-        public string Model { get; }
-        public string Color { get; set; }
-        public float Volume {
-            get { return volume; }
-            set {
-                if (value > 0)
-                    volume = value;
-            }
-        }
-        public int Min_t {
-            get { return min_t; }
-            set{
-                if (value <= 0)
-                    min_t = value;
-            }
-        }
-        public int Max_t {
-            get { return max_t; }
-            set{
-                if (value <= 0)
-                    max_t = value;
-            }
-        }
-    }
+    
 }
--- 2024-06-17 - HW (Intro to OOP)/Program.cs
+++ 2024-06-17 - HW (Intro to OOP)/Program.cs
@@ -1,34 +1,75 @@
-﻿namespace _2024_06_17___HW__Intro_to_OOP_
+﻿using System.Drawing;
+using System.Reflection;
+
+namespace _2024_06_17___HW__Intro_to_OOP_
 {
-    internal class Freezer
+    class Freezer
     {
         private string model;
         private string color;
-        private float volume;
+        private int volume;
         private int min_t;
         private int max_t;
+
         public Freezer()
         {
             model = "no model";
             color = "no color";
-            volume = 20;
-            min_t = 0;
-            max_t = 0;
+            Volume = 20;
+            Min_t = 0;
+            Max_t = 0;
         }
         public Freezer(string model) : base()
         {
-            Model = model;
+            this.model = model;
         }
         public Freezer(string model, string color) : this(model)
         {
-            Color = color;
+            this.color = color;
         }
-    }
+
+        public string Model { get; }
+        public string Color { get; set; }
+        public int Volume
+        {
+            get { return volume; }
+            set
+            {
+                if (value > 0)
+                    volume = value;
+            }
+        }
+        public int Min_t
+        {
+            get { return min_t; }
+            set
+            {
+                if (value <= 0)
+                    min_t = value;
+            }
+        }
+        public int Max_t
+        {
+            get { return max_t; }
+            set
+            {
+                if (value <= 0)
+                    max_t = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Model : {model}, Color : {color}, Volume : {volume}, Min temperature : {min_t}, Max temperature : {max_t}";
+        }
+    };
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Freezer freezer = new Freezer("freezer", "black");
+            string string_freezer = freezer.ToString();
+            Console.WriteLine(string_freezer);
         }
     }
 }
