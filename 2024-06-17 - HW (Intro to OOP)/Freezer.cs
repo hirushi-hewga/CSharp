717332300ec1367fe779071e57f8f284c99666b1
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-06-29 00:26:28 -0700

    HW (Intro_To_OOP)

---
+++ 2024-06-17 - HW (Intro to OOP)/Freezer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024_06_17___HW__Intro_to_OOP_
+{
+    internal class Freezer
+    {
+        public string Model { get; }
+        public string Color { get; set; }
+        public float Volume {
+            get { return volume; }
+            set {
+                if (value > 0)
+                    volume = value;
+            }
+        }
+        public int Min_t {
+            get { return min_t; }
+            set{
+                if (value <= 0)
+                    min_t = value;
+            }
+        }
+        public int Max_t {
+            get { return max_t; }
+            set{
+                if (value <= 0)
+                    max_t = value;
+            }
+        }
+    }
+}
---
+++ 2024-06-17 - HW (Intro to OOP)/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _2024_06_17___HW__Intro_to_OOP_
+{
+    internal class Freezer
+    {
+        private string model;
+        private string color;
+        private float volume;
+        private int min_t;
+        private int max_t;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
