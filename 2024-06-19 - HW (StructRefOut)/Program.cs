ab1ff315eb1a1b3aaa12e9b1feca988f4a58f7d6
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-07-01 16:28:07 -0700

    HW (Struct_Ref_Out)

--- 2024-06-19 - HW (StructRefOut)/Program.cs
+++ 2024-06-19 - HW (StructRefOut)/Program.cs
@@ -1,10 +1,27 @@
 ﻿namespace _2024_06_19___HW__StructRefOut_
 {
+    class Worker
+    {
+        string name;
+        string surname;
+        string lastname;
+        public string Name
+        {
+            private get { return Name; }
+            set {  }
+        }
+        public Worker()
+        {
+            name = null;
+            surname = null;
+            lastname = null;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
