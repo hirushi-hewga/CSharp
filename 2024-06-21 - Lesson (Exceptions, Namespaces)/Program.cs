b8978f271f66a7cea530b6f35492b923750dfdbd
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-07-03 07:29:07 -0700

    Lesson (Exceptions_Namespaces)

---
+++ 2024-06-21 - Lesson (Exceptions, Namespaces)/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _2024_06_21___Lesson__Exceptions__Namespaces_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1, n = 2;
+            try
+            {
+                Console.Write("Enter a : ");
+                a = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter b : ");
+                n = int.Parse(Console.ReadLine());
+
+                a = a / n;
+
+                Console.WriteLine("a = {0}", a);
+            }
+            catch (DivideByZeroException e)
+            {
+
+            }
+        }
+    }
+}
