f9802741e799a5afde61200f5adfd4ababe85bfd
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-07-03 20:56:37 -0700

    HW (Exceptions_Namespaces)

---
+++ 2024-06-21 - HW (Exceptions, Namespaces)/CreditCard.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2024_06_21___HW__Exceptions__Namespaces_
+{
+    struct Fullname
+    {
+        public string name;
+        public string surname;
+        public string lastname;
+        public Fullname(string name, string surname, string lastname)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.lastname = lastname;
+        }
+    }
+    partial class CreditCard
+    {
+        private string card_number;
+        private Fullname owner_full_name;
+        private string cvc;
+        private DateTime card_expiration_date;
+        public CreditCard(string card_number, Fullname owner_full_name, string cvc, DateTime card_expiration_date)
+        {
+            Card_number = card_number;
+            Owner_full_name = owner_full_name;
+            Cvc = cvc;
+            Card_expiration_date = card_expiration_date;
+        }
+    }
+    partial class CreditCard
+    {
+        public string Card_number
+        {
+            get { return card_number; }
+            set
+            {
+                if (card_number.Length != 16)
+                    throw new LengthException("Length exception. Length should be 16");
+                else if (!value.All(s => char.IsDigit(s)))
+                    throw new FormatException("Data type exception. Number must consist of numbers");
+                else card_number = value;
+            }
+        }
+        public Fullname Owner_full_name
+        {
+            get { return owner_full_name; }
+            set {
+                if (string.IsNullOrEmpty(value.name) || string.IsNullOrEmpty(value.surname) || string.IsNullOrEmpty(value.lastname))
+                    throw new ArgumentNullException("Argument null exception. Name, surname or lastname is empty");
+                else owner_full_name = value;
+            }
+        }
+        public string Cvc
+        {
+            get { return cvc; }
+            set
+            {
+                if (cvc.Length != 3)
+                    throw new LengthException("Length exception. Length should be 3");
+                else if (!value.All(s => char.IsDigit(s)))
+                    throw new FormatException("Data type exception. CVC must consist of numbers");
+                else card_number = value;
+            }
+        }
+        public DateTime Card_expiration_date
+        {
+            get { return card_expiration_date; }
+            set {
+                if (value <= DateTime.Now)
+                    throw new InvalidValueException("Invalid value exception. Date must be greater than now");
+                else card_expiration_date = value;
+            }
+        }
+    }
+}
---
+++ 2024-06-21 - HW (Exceptions, Namespaces)/Program.cs
@@ -0,0 +1,59 @@
+﻿using static System.Console;
+namespace _2024_06_21___HW__Exceptions__Namespaces_
+{
+    class LengthException : Exception
+    {
+        public LengthException(string message) : base(message) { }
+    }
+    class InvalidValueException : Exception
+    {
+        public InvalidValueException(string message) : base(message) { }
+    }
+    partial class CreditCard
+    {
+        public override string ToString()
+        {
+            return $"Card number : {Card_number}  |  Owner fullname : {Owner_full_name.surname}-{Owner_full_name.name}-{Owner_full_name.lastname}  |  CVC : {Cvc}  |  Card expiration date : {Card_expiration_date.ToLongDateString()}";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 1
+			try
+			{
+                Write("Enter number : ");
+                string string_value = ReadLine();
+                int value = int.Parse(string_value);
+                WriteLine(value);
+			}
+			catch (OverflowException ex)
+			{
+                WriteLine("Out of range exception");
+			}
+            catch (FormatException ex)
+            {
+                WriteLine("Data type exception");
+            }
+            catch (Exception ex)
+            {
+                WriteLine("Unknown exception");
+            }
+            */
+
+
+
+            /* 2*/
+            CreditCard[] cards =
+            {
+                new CreditCard("1111222233334444", new Fullname( "Andriy", "Ivanchuk", "Volodymyrovych" ), "123", new DateTime(2027, 11, 24)),
+                new CreditCard("4444555566667777", new Fullname( "Oksana", "Lasich", "Yuriivna" ), "456", new DateTime(2028, 12, 23))
+            };
+            foreach (CreditCard card in cards)
+            {
+                WriteLine(card.ToString());
+            }
+        }
+    }
+}
