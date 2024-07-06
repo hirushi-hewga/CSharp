5026b76f2c15a3c09ab9448e0dcc7874b4414579
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-07-06 22:11:50 +0300

    HW (Inheritance)

---
+++ 2024-06-26 - HW (Inheritance)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.AccessControl;
+using static System.Console;
+namespace _2024_06_26___HW__Inheritance_
+{
+    abstract class Shape
+    {
+        public abstract float GetArea();
+        public abstract float GetPerimetr();
+    }
+    class Triangle : Shape
+    {
+        public override float GetArea()
+        {
+            throw new NotImplementedException();
+        }
+        public override float GetPerimetr()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
