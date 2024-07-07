f28d29761c62d6ac36c902e9b1c52333b8dffc36
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-07-07 00:57:23 -0700

    HW (Inheritance)

--- 2024-06-26 - HW (Inheritance)/Program.cs
+++ 2024-06-26 - HW (Inheritance)/Program.cs
@@ -12,10 +12,11 @@ namespace _2024_06_26___HW__Inheritance_
     {
         private float side_a;
         private float side_b;
+        private float side_c;
         public float Side_a
         {
             get { return side_a; }
-            set {
+            private set {
                 if (value > 0)
                     side_a = value;
                 else
@@ -25,313 +26,327 @@ namespace _2024_06_26___HW__Inheritance_
         public float Side_b
         {
             get { return side_b; }
-            set {
+            private set {
                 if (value > 0)
                     side_b = value;
                 else
                     throw new ArgumentException("Argument exception. Side cannot be equal or less than zero");
             }
         }
-        public Triangle() : this(1, 1) { }
-        public Triangle(float side_a, float side_b)
+        public float Side_c
+        {
+            get { return side_c; }
+            private set {
+                if (value > 0)
+                    side_c = value;
+                else
+                    throw new ArgumentException("Argument exception. Side cannot be equal or less than zero");
+            }
+        }
+        public Triangle() : this(1, 1, 1) { }
+        public Triangle(float side_a, float side_b, float side_c)
         {
             Side_a = side_a;
             Side_b = side_b;
+            Side_c = side_c;
         }
         public override float GetArea()
         {
-            return 1;
+            float s = (Side_a + Side_b + Side_c) / 2;
+            float area = (float)Math.Sqrt(s * (s - Side_a) * (s - Side_b) * (s - Side_c));
+            return area;
         }
         public override float GetPerimetr()
         {
-            return 1;
+            return Side_a + Side_b + Side_c;
         }
     }
     class Square : Shape
     {
-        private float side_a;
-        private float side_b;
-        public float Side_a
+        private float side;
+        public float Side
         {
-            get { return side_a; }
-            set
+            get { return side; }
+            private set
             {
                 if (value > 0)
-                    side_a = value;
+                    side = value;
                 else
                     throw new ArgumentException("Argument exception. Side cannot be equal or less than zero");
             }
         }
-        public float Side_b
+        public Square() : this(1) { }
+        public Square(float side)
         {
-            get { return side_b; }
-            set
-            {
-                if (value > 0)
-                    side_b = value;
-                else
-                    throw new ArgumentException("Argument exception. Side cannot be equal or less than zero");
-            }
-        }
-        public Square() : this(1, 1) { }
-        public Square(float side_a, float side_b)
-        {
-            Side_a = side_a;
-            Side_b = side_b;
+            Side = side;
         }
         public override float GetArea()
         {
-            return 1;
+            return Side * Side;
         }
         public override float GetPerimetr()
         {
-            return 1;
+            return Side * 4;
         }
     }
     class Diamond : Shape
     {
-        private float side_a;
-        private float side_b;
-        public float Side_a
+        private float side;
+        public float Side
         {
-            get { return side_a; }
-            set
+            get { return side; }
+            private set
             {
                 if (value > 0)
-                    side_a = value;
+                    side = value;
                 else
                     throw new ArgumentException("Argument exception. Side cannot be equal or less than zero");
             }
         }
-        public float Side_b
+        public Diamond() : this(1) { }
+        public Diamond(float side)
         {
-            get { return side_b; }
-            set
-            {
-                if (value > 0)
-                    side_b = value;
-                else
-                    throw new ArgumentException("Argument exception. Side cannot be equal or less than zero");
-            }
-        }
-        public Diamond() : this(1, 1) { }
-        public Diamond(float side_a, float side_b)
-        {
-            Side_a = side_a;
-            Side_b = side_b;
+            Side = side;
         }
         public override float GetArea()
         {
-            return 1;
+            return (Side * Side) / 2;
         }
         public override float GetPerimetr()
         {
-            return 1;
+            return Side * 4;
         }
     }
     class Rectangle : Shape
     {
-        private float side_a;
-        private float side_b;
-        public float Side_a
+        private float base_length;
+        private float side_length;
+        public float Base_length
         {
-            get { return side_a; }
-            set
+            get { return base_length; }
+            private set
             {
                 if (value > 0)
-                    side_a = value;
+                    base_length = value;
                 else
                     throw new ArgumentException("Argument exception. Side cannot be equal or less than zero");
             }
         }
-        public float Side_b
+        public float Side_length
         {
-            get { return side_b; }
-            set
+            get { return side_length; }
+            private set
             {
                 if (value > 0)
-                    side_b = value;
+                    side_length = value;
                 else
                     throw new ArgumentException("Argument exception. Side cannot be equal or less than zero");
             }
         }
         public Rectangle() : this(1, 1) { }
-        public Rectangle(float side_a, float side_b)
+        public Rectangle(float base_length, float side_length)
         {
-            Side_a = side_a;
-            Side_b = side_b;
+            Base_length = base_length;
+            Side_length = side_length;
         }
         public override float GetArea()
         {
-            return 1;
+            return Base_length * Side_length;
         }
         public override float GetPerimetr()
         {
-            return 1;
+            return (Base_length + Side_length) * 2;
         }
     }
     class Parallelogram : Shape
     {
-        private float side_a;
-        private float side_b;
-        public float Side_a
+        private float base_length;
+        private float side_length;
+        private float height;
+        public float Base_length
         {
-            get { return side_a; }
-            set
+            get { return base_length; }
+            private set
             {
                 if (value > 0)
-                    side_a = value;
+                    base_length = value;
                 else
                     throw new ArgumentException("Argument exception. Side cannot be equal or less than zero");
             }
         }
-        public float Side_b
+        public float Side_length
         {
-            get { return side_b; }
-            set
+            get { return side_length; }
+            private set
             {
                 if (value > 0)
-                    side_b = value;
+                    side_length = value;
                 else
                     throw new ArgumentException("Argument exception. Side cannot be equal or less than zero");
             }
         }
-        public Parallelogram() : this(1, 1) { }
-        public Parallelogram(float side_a, float side_b)
+        public float Height
         {
-            Side_a = side_a;
-            Side_b = side_b;
+            get { return height; }
+            private set
+            {
+                if (value > 0)
+                    height = value;
+                else
+                    throw new ArgumentException("Argument exception. Side cannot be equal or less than zero");
+            }
+        }
+        public Parallelogram() : this(1, 1, 1) { }
+        public Parallelogram(float base_length, float side_length, float height)
+        {
+            Base_length = base_length;
+            Side_length = side_length;
+            Height = height;
         }
         public override float GetArea()
         {
-            return 1;
+            return Base_length * Height;
         }
         public override float GetPerimetr()
         {
-            return 1;
+            return (Base_length + Side_length) * 2;
         }
     }
     class Trapeze : Shape
     {
-        private float side_a;
-        private float side_b;
-        public float Side_a
+        private float base_length_1;
+        private float base_length_2;
+        private float side_length;
+        private float height;
+        public float Base_length_1
         {
-            get { return side_a; }
-            set
+            get { return base_length_1; }
+            private set
             {
                 if (value > 0)
-                    side_a = value;
+                    base_length_1 = value;
                 else
                     throw new ArgumentException("Argument exception. Side cannot be equal or less than zero");
             }
         }
-        public float Side_b
+        public float Base_length_2
         {
-            get { return side_b; }
-            set
+            get { return base_length_2; }
+            private set
             {
                 if (value > 0)
-                    side_b = value;
+                    base_length_2 = value;
                 else
                     throw new ArgumentException("Argument exception. Side cannot be equal or less than zero");
             }
         }
-        public Trapeze() : this(1, 1) { }
-        public Trapeze(float side_a, float side_b)
+        public float Side_length
         {
-            Side_a = side_a;
-            Side_b = side_b;
+            get { return side_length; }
+            private set
+            {
+                if (value > 0)
+                    side_length = value;
+                else
+                    throw new ArgumentException("Argument exception. Side cannot be equal or less than zero");
+            }
+        }
+        public float Height
+        {
+            get { return height; }
+            private set
+            {
+                if (value > 0)
+                    height = value;
+                else
+                    throw new ArgumentException("Argument exception. Side cannot be equal or less than zero");
+            }
+        }
+        public Trapeze() : this(1, 1, 1, 1) { }
+        public Trapeze(float base_length_1, float base_length_2, float side_length, float height)
+        {
+            Base_length_1 = base_length_1;
+            Base_length_2 = base_length_2;
+            Side_length = side_length;
+            Height = height;
         }
         public override float GetArea()
         {
-            return 1;
+            return (Base_length_1 + Base_length_2) / 2 * Height;
         }
         public override float GetPerimetr()
         {
-            return 1;
+            return (Side_length * 2) + Base_length_1 + Base_length_2;
         }
     }
     class Circle : Shape
     {
-        private float side_a;
-        private float side_b;
-        public float Side_a
+        private float radius;
+        public float Radius
         {
-            get { return side_a; }
-            set
+            get { return radius; }
+            private set
             {
                 if (value > 0)
-                    side_a = value;
+                    radius = value;
                 else
                     throw new ArgumentException("Argument exception. Side cannot be equal or less than zero");
             }
         }
-        public float Side_b
+        public Circle() : this(1) { }
+        public Circle(float radius)
         {
-            get { return side_b; }
-            set
-            {
-                if (value > 0)
-                    side_b = value;
-                else
-                    throw new ArgumentException("Argument exception. Side cannot be equal or less than zero");
-            }
-        }
-        public Circle() : this(1, 1) { }
-        public Circle(float side_a, float side_b)
-        {
-            Side_a = side_a;
-            Side_b = side_b;
+            Radius = radius;
         }
         public override float GetArea()
         {
-            return 1;
+            return (float)(Math.PI * Radius * Radius);
         }
         public override float GetPerimetr()
         {
-            return 1;
+            return (float)(2 * Math.PI * Radius);
         }
     }
     class Ellipse : Shape
     {
-        private float side_a;
-        private float side_b;
-        public float Side_a
+        private float base_axis;
+        private float additional_axis;
+        public float Base_axis
         {
-            get { return side_a; }
-            set
+            get { return base_axis; }
+            private set
             {
                 if (value > 0)
-                    side_a = value;
+                    base_axis = value;
                 else
                     throw new ArgumentException("Argument exception. Side cannot be equal or less than zero");
             }
         }
-        public float Side_b
+        public float Additional_axis
         {
-            get { return side_b; }
-            set
+            get { return additional_axis; }
+            private set
             {
                 if (value > 0)
-                    side_b = value;
+                    additional_axis = value;
                 else
                     throw new ArgumentException("Argument exception. Side cannot be equal or less than zero");
             }
         }
         public Ellipse() : this(1, 1) { }
-        public Ellipse(float side_a, float side_b)
+        public Ellipse(float base_axis, float additional_axis)
         {
-            Side_a = side_a;
-            Side_b = side_b;
+            Base_axis = base_axis;
+            Additional_axis = additional_axis;
         }
         public override float GetArea()
         {
-            return 1;
+            return (float)(Math.PI * Base_axis * Additional_axis);
         }
         public override float GetPerimetr()
         {
-            return 1;
+            double h = Math.Pow((Base_axis - Additional_axis), 2) / Math.Pow((Base_axis + Additional_axis), 2);
+            return (float)(Math.PI * (Base_axis + Additional_axis) * (1 + (3 * h) / (10 + Math.Sqrt(4 - 3 * h))));
         }
     }
     internal class Program
