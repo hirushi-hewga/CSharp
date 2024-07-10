f87fe323dfd90756b48e5ee0718b98924453aecc
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-07-10 09:09:39 -0700

    HW (StandartInterfaces)

---
+++ 2024-07-01 - HW (Standart interfaces)/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _2024_07_01___HW__Standart_interfaces_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
