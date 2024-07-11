7e79dab2c3bf111317189e58b57f9cddcc58d29b
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-07-10 19:38:13 -0700

    HW (StandartInterfaces)

--- 2024-07-01 - HW (Standart interfaces)/Program.cs
+++ 2024-07-01 - HW (Standart interfaces)/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace _2024_07_01___HW__Standart_interfaces_
+﻿using System.Security.AccessControl;
+
+namespace _2024_07_01___HW__Standart_interfaces_
 {
+    class Director
+    {
+
+    }
+    class Movie
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Director director { get; set; }
+        public string Country { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+        public int Rating { get; set; }
+    }
+    class Cinema
+    {
+
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
