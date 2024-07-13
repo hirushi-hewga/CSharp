54ad1270934da1f7b356b56b28c6181dd562a1fc
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-07-12 18:30:14 -0700

    Lesson (Delegates)

---
+++ 2024-07-02 - Lesson (Delegates)/Program.cs
@@ -0,0 +1,71 @@
+﻿using static System.Console;
+namespace _2024_07_02___Lesson__Delegates_
+{
+    //class MyDelegate : Delegate
+    //{
+    //
+    //}
+    //public delegate void VoidDelegate();
+    //public delegate int IntDelegate(double value);
+
+    public delegate void SetStringDelegate(string value);
+    public delegate double DoubleDelegate();
+    public delegate void VoidDelegate();
+    class SuperClass
+    {
+        public void Print(string str)
+        {
+            WriteLine(str + "!!!");
+        }
+        public static double GetKoef()
+        {
+            double res = new Random().NextDouble();
+            return res;
+        }
+        public double GetNumber()
+        {
+            return new Random().Next();
+        }
+        public void DoWork()
+        {
+            WriteLine("I do some work");
+        }
+        public void Test()
+        {
+            WriteLine("I testing");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SuperClass superClass = new SuperClass();
+            superClass.Test();
+            SuperClass.GetKoef();
+
+            DoubleDelegate method = new DoubleDelegate(SuperClass.GetKoef);
+
+            //if (method == null)
+            //{
+            //    WriteLine("method is null");
+            //}
+            //else
+            //{
+            //    WriteLine(method());
+            //}
+
+            WriteLine(method?.Invoke());
+
+            DoubleDelegate[] arr = new DoubleDelegate[]
+            {
+                SuperClass.GetKoef,
+                superClass.GetNumber
+            };
+            WriteLine(arr[0].Invoke());
+            WriteLine(arr[1]());
+
+            SetStringDelegate stringDelegate = new SetStringDelegate(superClass.Print);
+            VoidDelegate @void = new VoidDelegate(superClass.Print)
+        }
+    }
+}
