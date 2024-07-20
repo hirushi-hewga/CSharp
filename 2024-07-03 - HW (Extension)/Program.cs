cb1d5c24b419bb8a250ea7bf9ad2eeb3e9453c25
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-07-20 02:16:59 -0700

    HW (Extension)

---
+++ 2024-07-03 - HW (Extension)/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _2024_07_03___HW__Extension_
+{
+    static class Extension
+    {
+        public static bool IsPalindrome(this string word)
+        {
+            bool isPalindrome = true;
+            for (int i = 0; i < word.Length / 2; i++)
+            {
+                if (word[i] != word[word.Length - i - 1])
+                    isPalindrome = false;
+            }
+            return isPalindrome;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region IsPalindrome
+
+            /*
+            string word;
+            Console.Write("Enter word : ");
+            word = Console.ReadLine();
+            if (word.IsPalindrome())
+                Console.WriteLine("\nThis word is palindrome");
+            else
+                Console.WriteLine("\nThis word is not palindrome");
+            */
+
+            #endregion
+
+            #region Encryption
+
+            /**/
+            string word;
+            Console.Write("Enter word : ");
+            word = Console.ReadLine();
+
+
+            #endregion
+        }
+    }
+}
