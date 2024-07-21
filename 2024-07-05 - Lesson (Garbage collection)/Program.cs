d0099ed10a1f1317060c158c6d19da70b8f69b2e
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-07-21 09:43:56 -0700

    Lesson (GC)

---
+++ 2024-07-05 - Lesson (Garbage collection)/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+namespace _2024_07_05___Lesson__Garbage_collection_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int a = 5;
+            string str = "1";
+            string copy = null;
+            if (true)
+            {
+                int b = 10;
+                string str2 = "2";
+                //delete str2;
+                copy = str2;
+                Console.WriteLine(str2);
+                Console.WriteLine(b);
+            }
+            //Console.WriteLine(b);
+            //Console.WriteLine(str2);
+            Console.WriteLine(copy);
+            Console.WriteLine(copy + str);
+            copy = "Hello";
+            */
+
+
+
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("Демонстрація System.GC");
+            Console.WriteLine($"Максимальне покоління : {GC.MaxGeneration}");
+            //GarbageHelper hlp = new GarbageHelper();
+
+            // Покоління, в якому знаходиться об'єкт
+            //Console.WriteLine($"Покоління об'єкта : {GC.GetGeneration(hlp)}");
+            // Кількість зарезервованої пам'яті
+            Console.WriteLine($"Зарезервовано пам'яті (байт) : {GC.GetTotalMemory(false)}");
+            // Створення сміття
+            //hlp.MakeGarbage();
+            Console.WriteLine($"Зарезервовано пам'яті (байт) : {GC.GetTotalMemory(false)}");
+        }
+    }
+}
