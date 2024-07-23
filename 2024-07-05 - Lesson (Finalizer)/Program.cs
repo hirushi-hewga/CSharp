e1fb610fe011648fee2f438b28c25292ebe47396
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-07-23 02:25:47 -0700

    Lesson (Finalizer)

--- 2024-07-05 - Lesson (Finalizer)/Program.cs
+++ 2024-07-05 - Lesson (Finalizer)/Program.cs
@@ -1,10 +1,28 @@
 ﻿namespace _2024_07_05___Lesson__Finalizer_
 {
+    class TestClass
+    {
+        public int MyProperty { get; set; }
+        public TestClass()
+        {
+            Console.WriteLine("Constructor!");
+        }
+        // protected override void Finalize();
+        ~TestClass()
+        {
+            Console.WriteLine("Finalize!");
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            if (true)
+            {
+                TestClass test = new TestClass();
+            }
+
+            Console.ReadKey();
         }
     }
 }
