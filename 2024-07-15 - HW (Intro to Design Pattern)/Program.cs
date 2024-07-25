fc25ec7ca2ab1a30924430d54993d2047f45a5d0
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-07-25 13:41:52 -0700

    HW (Intro to Design Pattern)

---
+++ 2024-07-15 - HW (Intro to Design Pattern)/Program.cs
@@ -0,0 +1,92 @@
+﻿using static System.Console;
+namespace _2024_07_15___HW__Intro_to_Design_Pattern_
+{
+    abstract class Processor
+    {
+        public string Name { get; set; }
+        public Processor(string name)
+        {
+            Name = name;
+        }
+        public abstract void Print();
+        public override string ToString()
+        {
+            return $"Name : {Name}";
+        }
+    }
+    abstract class MainBoard
+    {
+        public string Name { get; set; }
+        public MainBoard(string name)
+        {
+            Name = name;
+        }
+        public abstract void Print();
+        public override string ToString()
+        {
+            return $"Name : {Name}";
+        }
+    }
+    abstract class Box
+    {
+        public string Name { get; set; }
+        public Box(string name)
+        {
+            Name = name;
+        }
+        public abstract void Print();
+        public override string ToString()
+        {
+            return $"Name : {Name}";
+        }
+    }
+    abstract class Hdd
+    {
+        public string Name { get; set; }
+        public Hdd(string name)
+        {
+            Name = name;
+        }
+        public abstract void Print();
+        public override string ToString()
+        {
+            return $"Name : {Name}";
+        }
+    }
+    abstract class Memory
+    {
+        public string Name { get; set; }
+        public Memory(string name)
+        {
+            Name = name;
+        }
+        public abstract void Print();
+        public override string ToString()
+        {
+            return $"Name : {Name}";
+        }
+    }
+    class AmdProcessor : Processor
+    {
+        public AmdProcessor(string name) : base(name) { }
+        public override void Print()
+        {
+            WriteLine(ToString());
+        }
+    }
+    class AsusMainBord : MainBoard
+    {
+        public AsusMainBord(string name) : base(name) { }
+        public override void Print()
+        {
+            WriteLine(ToString());
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
