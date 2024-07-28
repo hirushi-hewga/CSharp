4f7c922b82dca4e84a65b82e5c1d116102437179
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-07-28 16:46:48 -0700

    HW (Dictionary)

--- 2024-07-05 - HW (Dictionary)/Program.cs
+++ 2024-07-05 - HW (Dictionary)/Program.cs
@@ -2,8 +2,35 @@
 {
     class User
     {
-        public string Name { get; set; }
-        public string Surname { get; set; }
+        private string name;
+        private string surname;
+        public string Name {
+            get { return name; }
+            set {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentNullException("name");
+                name = value;
+            }
+        }
+        public string Surname
+        {
+            get { return surname; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentNullException("surname");
+                surname = value;
+            }
+        }
+        public User(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+        public override string ToString()
+        {
+            return $"{name} {surname}";
+        }
     }
     class PhoneBook
     {
@@ -14,18 +41,102 @@
         }
         public void AddPhone(string phone_number, string user_name, string user_surname)
         {
-            if (!phoneBook.ContainsKey(phone_number))
+            if (phoneBook.ContainsKey(phone_number))
             {
-                phoneBook.Add(phone_number, new User { Name = user_name, Surname = user_surname });
+                Console.WriteLine($"\nNumber {phone_number} is already in the phone book\n");
             }
-            else { Console.WriteLine($"\nNumber {phone_number} is already in the phone book\n"); }
+            else if (string.IsNullOrEmpty(phone_number))
+            {
+                throw new ArgumentNullException("phone number");
+            }
+            else { phoneBook.Add(phone_number, new User(user_name, user_surname)); }
+        }
+        public void ChangeName(string phone_number, string user_name)
+        {
+            phoneBook[phone_number].Name = user_name;
+        }
+        public void ChangeSurname(string phone_number, string user_surname)
+        {
+            phoneBook[phone_number].Surname = user_surname;
+        }
+        public void SearchByPhone(string phone_number)
+        {
+            if (!phoneBook.ContainsKey(phone_number))
+                Console.WriteLine($"\nNumber {phone_number} not found in the phone book\n");
+            else
+                Console.WriteLine($"{phone_number} - {phoneBook[phone_number].ToString()}");
+        }
+        public void SearchByName(string user_name)
+        {
+            bool isFound = false;
+            foreach (var item in phoneBook)
+            {
+                if (item.Value.Name == user_name)
+                {
+                    Console.WriteLine($"{item.Key} - {item.Value.ToString()}");
+                    isFound = true;
+                    break;
+                }
+            }
+            if (!isFound)
+            {
+                Console.WriteLine($"\nName {user_name} not found in the phone book\n");
+            }
+        }
+        public void SearchBySurname(string user_surname)
+        {
+            bool isFound = false;
+            foreach (var item in phoneBook)
+            {
+                if (item.Value.Surname == user_surname)
+                {
+                    Console.WriteLine($"{item.Key} - {item.Value.ToString()}");
+                    isFound = true;
+                    break;
+                }
+            }
+            if (!isFound)
+            {
+                Console.WriteLine($"\nSurname {user_surname} not found in the phone book\n");
+            }
+        }
+        public void DeleteByNumber(string phone_number)
+        {
+            phoneBook.Remove(phone_number);
+        }
+        public void ShowAll()
+        {
+            Console.WriteLine("=================== All ==================");
+            foreach (var item in phoneBook)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value.ToString()}");
+            }
+            Console.WriteLine("==========================================");
         }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            PhoneBook phoneBook = new PhoneBook();
+            try
+            {
+                phoneBook.AddPhone("123456789", "Olga", "Petruk");
+                phoneBook.AddPhone("124555457", "Ilya", "Gorchak");
+                phoneBook.AddPhone("323667856", "Ivan", "Grozniy");
+                phoneBook.AddPhone("987654321", "Petro", "Shevchenko");
+            }
+            catch (ArgumentNullException ex) { Console.WriteLine(ex.Message); }
+            catch (Exception ex) { Console.WriteLine("Unknown exception..."); }
+            phoneBook.ShowAll();
+            phoneBook.ChangeName("124555457", "Amadey");
+            phoneBook.ChangeSurname("123456789", "Tuz");
+            phoneBook.ShowAll();
+            phoneBook.SearchByPhone("123456789");
+            phoneBook.SearchByName("Amadey");
+            phoneBook.SearchBySurname("Shevchenko");
+            phoneBook.DeleteByNumber("323667856");
+            phoneBook.ShowAll();
         }
     }
 }
